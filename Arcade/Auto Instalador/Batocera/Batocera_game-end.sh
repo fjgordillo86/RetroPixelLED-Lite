b17db8950fb236a1bac55f2102f8cea9823a409d
fjgordillo86 <59791834+fjgordillo86@users.noreply.github.com>
2026-08-02 12:55:43 +0200

    Delete Arcade/Auto Instalador directory

--- Arcade/Auto Instalador/Batocera/Batocera_game-end.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Colocar en: /userdata/system/configs/emulationstation/scripts/game-end/
-# (Opcional) Envía STOP al ESP32 cuando sales de un juego y vuelves al menú.
-
-IP_ESP32="192.168.1.117"
-PORT_ESP32=8888
-SENDER="/userdata/userscripts/Batocera_marquesina.py"
-
-python3 "$SENDER" "$IP_ESP32" "$PORT_ESP32" "STOP" &
