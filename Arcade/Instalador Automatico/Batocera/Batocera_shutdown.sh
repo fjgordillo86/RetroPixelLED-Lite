85a1fdbba46cdfc5c81ce69759b825b4c8e104f9
fjgordillo86 <59791834+fjgordillo86@users.noreply.github.com>
2026-08-24 13:42:12 +0200

    Delete Arcade/Instalador Automatico directory

--- Arcade/Instalador Automatico/Batocera/Batocera_shutdown.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Colocar en: /userdata/system/configs/emulationstation/scripts/quit/
-# Se dispara al apagar Batocera de forma normal. Avisa al panel para que deje la marquesina.
-
-IP_ESP32="192.168.1.117"
-PORT_ESP32=8888
-SENDER="/userdata/userscripts/Batocera_marquesina.py"
-
-python3 "$SENDER" "$IP_ESP32" "$PORT_ESP32" "STOP"
